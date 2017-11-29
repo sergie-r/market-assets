ebe2bd22fc0af3bbe56efebe968d59de6239572e
Sergey <ryabchenkoserge@gmail.com>
2017-11-29 21:03:30 +0200

    remove node_modules

--- node_modules/react-virtual-list/src/utils/topFromWindow.js
+++
@@ -1,7 +0,0 @@
-const topFromWindow = (element) => {
-  if (typeof element === 'undefined' || !element) return 0;
-  
-  return (element.offsetTop || 0) + topFromWindow(element.offsetParent);
-};
-
-export default topFromWindow;
